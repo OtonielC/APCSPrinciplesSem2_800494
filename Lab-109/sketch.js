c615eb482a2d7751fc88d58a1e2673996fb9efeb
OtonielC <800494@seq.org>
2019-01-15 08:50:16 -0800

    1/15 metrics

--- Lab-109/sketch.js
+++ Lab-109/sketch.js
@@ -4,7 +4,7 @@
 
 var numofcompares = 0;
 var numofswaps = 0;
-var time = 0;
+var num = 100
 var x = []
 //  The setup function function is called once when your program begins
 function setup() {
@@ -12,28 +12,29 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  for(var i = 0; i < 5; i++){
-    x[i] = round(random(5))
+  for(var i = 0; i < num; i++){
+    x[i] = round(random(num))
   }
   insertSort();
   // mySort();
   // bubbleSort(x);
   // selectionSort(x);
-  console.log(numofswaps);
-  console.log(numofcompares);
-  console.log(time);
+  time();
+  console.log('the number of swaps was ' + numofswaps + '.');
+  console.log('the number of compares was ' + numofcompares + '.');
 }
 
-//  The draw function is called @ 30 fps
-function draw() {
-
+function time(){
+  var millisecond = millis();
+  text('Milliseconds \nrunning: \n' + millisecond, 5, 40);
+  console.log('this function ran in ' + millisecond + ' milliseconds.');
 }
 
 function swap(items, firstIndex, secondIndex){
-    var temp = items[firstIndex];
-    items[firstIndex] = items[secondIndex];
-    items[secondIndex] = temp;
-    numofswaps+=1
+  var temp = items[firstIndex];
+  items[firstIndex] = items[secondIndex];
+  items[secondIndex] = temp;
+  numofswaps+=1
 }
 
 
@@ -55,10 +56,11 @@ function swap(items, firstIndex, secondIndex){
 
 
 function bubbleSort(x) {
+
   for(var i = 0; i < x.length; i++) {
     for(var j = 1; j < x.length; j++) {
+      numofcompares+=1
       if(x[j - 1] > x[j]) {
-        numofcompares+=1;
         swap(x, j - 1, j);
       }
     }
@@ -72,26 +74,27 @@ function bubbleSort(x) {
 
 
 function selectionSort(items){
-    var len = items.length,
-        min;
-    for (var i = 0; i < len; i++){
-        //set minimum to this position aka whatever position i is
-        min = i;
-        //check the rest of the array to see if anything is smaller if not then
-        //jump out of this loop and continue to the next traverse in the array.
-        for(var j = i + 1; j < len; j++){
-            if (items[j] < items[min]){
-              numofcompares+=1
-              min = j;
-            }
-        }
-        //if the minimum isn't in the position, swap it
-        if (i != min){
-            swap(items, i, min);
-        }
-        console.log(x);
+  var len = items.length,
+  min;
+  for (var i = 0; i < len; i++){
+    //set minimum to this position aka whatever position i is
+    min = i;
+    //check the rest of the array to see if anything is smaller if not then
+    //jump out of this loop and continue to the next traverse in the array.
+    for(var j = i + 1; j < len; j++){
+      numofcompares+=1
+      if (items[j] < items[min]){
+        numofcompares+=1
+        min = j;
+      }
     }
-    return items;
+    //if the minimum isn't in the position, swap it
+    if (i != min){
+      swap(items, i, min);
+    }
+    console.log(x);
+  }
+  return items;
 }
 
 
@@ -103,8 +106,8 @@ function selectionSort(items){
 function insertSort(){
   for(var i = 1; i < x.length; i++){
     for(var j = i; j > 0; j--){
+      numofcompares+=1;
       if(x[j] < x[j-1]){
-        numofcompares+=1;
         swap(x, j, j-1)
       }
     }
