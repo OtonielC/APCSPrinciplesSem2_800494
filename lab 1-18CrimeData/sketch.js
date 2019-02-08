1ecbba81d8fa6e9ad24a8772fe31ae4ce4894864
OtonielC <800494@seq.org>
2019-02-08 08:49:45 -0800

    2/8

--- lab 1-18CrimeData/sketch.js
+++ lab 1-18CrimeData/sketch.js
@@ -2,7 +2,7 @@
 //Otoniel Carreon
 //APCS
 //10/23
-var run = false;
+var run = true;
 
 //  The setup function function is called once when your program begins
 function setup() {
@@ -10,16 +10,33 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  run = true;
-  assaultSort();
-  murderSort();
+  selectursort();
 }
 
 function draw(){
-  fill(255);
+  //home base
+  fill(0,255,0);
   rect(25,25,50,50)
 }
-
+function selectursort(){
+  if(run === true){
+    //infield
+    fill(255,51,153)
+    rect(50,50,700,700)
+    //outfield
+    fill(139,69,19)
+    rect(50,50,350,350)
+    //second base
+    fill(255)
+    rect(25,350,50,50)
+    //third base
+    fill(255)
+    rect(350,350,50,50)
+    //fourth base
+    fill(255)
+    rect(350,25,50,50)
+  }
+}
 
 function swap(items, firstIndex, secondIndex){
   var temp = items[firstIndex];
@@ -52,7 +69,3 @@ function murderSort(){
   console.log(data.crime);
   return data.crime;
 }
-
-// function mousePressed(){
-//   if()
-// }
