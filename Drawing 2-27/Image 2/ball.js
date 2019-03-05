45b081ed7f413985b0b49da36fa2437bbcae6132
OtonielC <800494@seq.org>
2019-03-05 08:49:31 -0800

    3/5/19

---
+++ Drawing 2-27/Image 2/ball.js
@@ -0,0 +1,55 @@
+function ball (loc, vel, radius, col,acc){
+  this.loc = loc;
+  this.vel = vel;
+  this.rad = radius;
+  this.col = color(random(255),random(255),random(255),random(255));
+  this.acc = acc;
+
+  this.run = function(){
+    this.checkEdges();
+    this.update();
+    this.render();
+  }
+
+
+  this.update = function(){
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+    if(this !== b1){
+      var d = this.loc.dist(b1.loc)
+      if(d<3000){
+        var steeringForce = p5.Vector.sub(b1.loc, this.loc);
+        steeringForce.normalize();
+        steeringForce.mult(.2);
+        this.vel.add(steeringForce);
+      }
+      if(d < 100){
+        var steeringForce = p5.Vector.sub( this.loc, b1.loc);
+        steeringForce.normalize();
+        steeringForce.mult(0.2);
+        this.vel.add(steeringForce);
+      }
+    }
+  }
+
+
+  this.checkEdges = function(){
+    if(this.loc.x < 0) this.vel.x = -this.vel.x;
+    if(this.loc.x > width) this.vel.x = -this.vel.x;
+    if(this.loc.y < 0) this.vel.y = -this.vel.y;
+    if(this.loc.y > height) this.vel.y = -this.vel.y;
+  }
+
+
+  this.render = function(){
+    fill(this.col);
+    // ellipse(this.loc.x, this.loc.y, this.rad, this.rad);
+    if(toggle === true){
+      stroke(this.col)
+      for(var i = 1; i < balls.length; i++){
+        strokeWeight(100)
+        line(this.loc.x, this.loc.y, balls[i-1].loc.x, balls[i-1].loc.y)
+      }
+    }
+  }
+}
---
+++ Drawing 2-27/Image 3/ball.js
@@ -0,0 +1,60 @@
+function ball (loc, vel, radius, col,acc){
+  this.loc = loc;
+  this.vel = vel;
+  this.rad = radius;
+  this.col = color(random(255),random(255),random(255),random(255));
+  this.acc = acc;
+
+  this.run = function(){
+    this.checkEdges();
+    this.update();
+    this.render();
+  }
+
+
+  this.update = function(){
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+    // if(this !== b1){
+    //   var d = this.loc.dist(b1.loc)
+    //   if(d<3000){
+    //     var steeringForce = p5.Vector.sub(b1.loc, this.loc);
+    //     steeringForce.normalize();
+    //     steeringForce.mult(.2);
+    //     this.vel.add(steeringForce);
+    //   }
+    //   if(d < 100){
+    //     var steeringForce = p5.Vector.sub( this.loc, b1.loc);
+    //     steeringForce.normalize();
+    //     steeringForce.mult(0.2);
+    //     this.vel.add(steeringForce);
+    //   }
+    // }
+  }
+
+
+  this.checkEdges = function(){
+    if(this.loc.x < 0) this.vel.x = -this.vel.x;
+    if(this.loc.x > width) this.vel.x = -this.vel.x;
+    if(this.loc.y < 0) this.vel.y = -this.vel.y;
+    if(this.loc.y > height) this.vel.y = -this.vel.y;
+  }
+
+
+  this.render = function(){
+    fill(this.col);
+    // ellipse(this.loc.x, this.loc.y, this.rad, this.rad);
+    if(toggle === true){
+      stroke(this.col)
+      for(var i = 1; i < balls.length; i++){
+        ellipse(this.loc.x, this.loc.y, 50, 50)
+        if(balls[i].loc.x > 100 &&
+          balls[i].loc.x < 700 &&
+          balls[i].loc.y < 100 &&
+          balls[i].loc.y > 700){
+            fill(255)
+          }
+        }
+      }
+    }
+  }
